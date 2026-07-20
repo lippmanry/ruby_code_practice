3c46ff9e62b04d7cb27658483f977af70cd3e145
Ryan J Lippman <ryan@datacat-security.com>
2026-07-20 12:10:52 +0100

    fixed some incompatiblities

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
+#add products index as root route
+root "products#index"
 #creates all 8 CRUD actions
 resources :products
 
